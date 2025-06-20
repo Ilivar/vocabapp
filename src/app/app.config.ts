87530a7d3ed27888434725196a1ead95b1cffbec
Giuliano Gioia <giulianogioia86@gmail.com>
2025-06-20 07:30:46 +0200

    Step 1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "vocabapp-c42ef", appId: "1:405151128190:web:d075ffac309e8c9534d62f", storageBucket: "vocabapp-c42ef.firebasestorage.app", apiKey: "AIzaSyBbTk9PamX5N0iCpzxDpBG3L_6yymLDfLk", authDomain: "vocabapp-c42ef.firebaseapp.com", messagingSenderId: "405151128190" })), provideFirestore(() => getFirestore())
   ]
 };
---
+++ src/app/firebase.ts
@@ -0,0 +1,22 @@
+import { inject, Injectable } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { collection, onSnapshot } from 'firebase/firestore';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Firebase {
+  firestore: Firestore = inject(Firestore);
+  unsubscribe;
+
+  constructor() {
+    this.unsubscribe = onSnapshot(
+      collection(this.firestore, 'Vocabulary'),
+      (data) => {
+        data.forEach((doc) => {
+          console.log(doc.data());
+        });
+      }
+    );
+  }
+}
